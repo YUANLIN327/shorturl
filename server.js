47dc46f8e66d8b72473448ca2c4049dd2f36464e
YUANLIN327 <yuanlin.dalian@gmail.com>
2016-03-09 16:47:48 -0500

    Update Controller, server.js
    Realize function

--- server.js
+++ server.js
@@ -12,6 +12,62 @@ app.use(bodyParser.json());
 
 mongoose.connect("mongodb://localhost/shorturlwebs");
 
+//redirect users
+app.get("/:id", function(req, res) {
+
+  ShortWeb.findOne({
+    shortUrl: req.params.id
+  }, function(err, foundWeb) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundWeb) {
+        res.redirect("http://" + foundWeb.originWebsite);
+      } else {
+        res.redirect("/");
+      }
+
+    }
+  });
+
+});
+
+//handle post request
+app.post("/",function(req,res){
+  var origin = parse.parseUrl(req.body.originalUrl);
+  console.log(req.body.originalUrl);
+  ShortWeb.findOne({
+    'originWebsite': origin
+  }, function(err, website) {
+    if (err) {
+      console.log(err);
+    } else {
+      //if found
+      if (website) {
+        console.log(website);
+        res.json(website);
+      }
+      //if not found
+      else {
+
+        //create a new shortweb
+        var newShortWeb = {
+          originWebsite: origin,
+          shortUrl: random.generateValidateId()
+        };
+        ShortWeb.create(newShortWeb, function(err, newlyCreatedWeb) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(newlyCreatedWeb);
+            res.json(newlyCreatedWeb);
+          }
+        });
+      }
+    }
+  });
+});
+
 app.listen("2000",function(){
-  console.log("server start");
+  console.log("server is running");
 });
