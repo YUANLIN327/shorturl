c6ef9357f36293e9a5a8309d2e1113468671abfc
YUANLIN327 <yuanlin.dalian@gmail.com>
2016-03-09 16:23:33 -0500

    Add Controller

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,1 +1,23 @@
-$null 
+(function() {
+
+    var app = angular.module('myApp', []);
+
+    app.controller("AppCtrl",['$scope', '$http', function($scope, $http){
+      $scope.generate = function(){
+
+        var body = JSON.stringify({
+          originalUrl: $scope.originalUrl
+        });
+
+        console.log($scope.originWeb);
+        $http.post("/",body).success(function(response){
+          console.log("I got the data I requested");
+          $scope.shortUrl = response.shortUrl;
+        });
+      };
+
+
+    }]);
+
+})();
+ 
