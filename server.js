e05393ba4c650db28355d8d8e0d90b36e7ac63f0
YUANLIN327 <yuanlin.dalian@gmail.com>
2016-03-09 16:11:36 -0500

    commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express'),
+  mongoose = require("mongoose"),
+  bodyParser = require("body-Parser"),
+  app = express();
+
+app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost/shorturlwebs");
+
+app.listen("2000",function(){
+  console.log("server start");
+});
